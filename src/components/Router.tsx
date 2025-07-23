e57359f967a17d4fc58cbedb513c07069a4bd2ce
jiyoung.shin <eve0625@naver.com>
2025-07-23 20:33:24 +0900

    라우터는 별도 파일로 분리

---
+++ src/components/Router.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export default function Router() {
+  return (
+    <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/posts" element={<h1>Post List Page</h1>} />
+        <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
+        <Route path="/posts/new" element={<h1>New Post Page</h1>} />
+        <Route path="/posts/edit/:id" element={<h1>Post Edit Page</h1>} />
+        <Route path="/profile" element={<h1>Profile Page</h1>} />
+        <Route path="*" element={<Navigate to="/" replace />} /> {/* Default Path */}
+    </Routes>
+  );
+};
