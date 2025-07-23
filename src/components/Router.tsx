40afbf9d7d8e73474739add03c3b3f20ca9c861c
jiyoung.shin <eve0625@naver.com>
2025-07-23 20:52:35 +0900

    컴포넌트를 생성하여 라우트에 적용

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,14 +1,24 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from '../pages/home'
+import PostList from '../pages/posts';
+import PostDetail from '../pages/posts/detail';
+import PostNew from '../pages/posts/new';
+import PostEdit from '../pages/posts/edit';
+import Profile from '../pages/profile';
+import Login from '../pages/login';
+import SignUp from '../pages/signup';
 
 export default function Router() {
   return (
     <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/posts" element={<h1>Post List Page</h1>} />
-        <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
-        <Route path="/posts/new" element={<h1>New Post Page</h1>} />
-        <Route path="/posts/edit/:id" element={<h1>Post Edit Page</h1>} />
-        <Route path="/profile" element={<h1>Profile Page</h1>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/posts" element={<PostList/>} />
+        <Route path="/posts/:id" element={<PostDetail/>} />
+        <Route path="/posts/new" element={<PostNew/>} />
+        <Route path="/posts/edit/:id" element={<PostEdit/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
         <Route path="*" element={<Navigate to="/" replace />} /> {/* Default Path */}
     </Routes>
   );
