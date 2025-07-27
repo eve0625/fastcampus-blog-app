38b8cdc771870e411e1e48fa2ed90b547629833d
jiyoung.shin <eve0625@naver.com>
2025-07-27 14:40:29 +0900

    게시글 상세 및 헤더의 컴포넌트 작업

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,5 +1,7 @@
-export default function PostDetail() {
+import PostDetail from "../../components/PostDetail";
+
+export default function PostDetailPage() {
     return (
-        <h1>Post Detail Page</h1>
+        <PostDetail />
     );
 }
