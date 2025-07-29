d65a5395c40c5b4403e2d96178ad9ba2c6cf0a19
jiyoung.shin <eve0625@naver.com>
2025-07-29 16:47:56 +0900

    게시글 입력 폼

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export default function PostDetail() {
     return <>
         <div className="post__detail">
@@ -12,7 +14,9 @@ export default function PostDetail() {
                 </div>
                 <div className="post__utils-box">
                     <div className="post__delete">삭제</div>
-                    <div className="post__edit">수정</div>
+                    <div className="post__edit">
+                        <Link to={`/posts/edit/1`}>수정</Link>
+                    </div>
                 </div>
                 <div className="post__text">
                     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
